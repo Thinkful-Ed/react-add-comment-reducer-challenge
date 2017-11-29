94b86c48c238698cf54a17a6c05568e599ddc97b
Joe Turner <joe@oampo.co.uk>
2017-11-29 13:57:58 +0000

    Add starting point

--- src/reducer.js
+++ src/reducer.js
@@ -4,17 +4,4 @@ const initialState = {
     comments: []
 };
 
-export default (state = initialState, action) => {
-    if (action.type === ADD_COMMENT) {
-        return Object.assign({}, state, {
-            comments: [
-                ...state.comments,
-                {
-                    author: action.author,
-                    comment: action.comment
-                }
-            ]
-        });
-    }
-    return state;
-};
+export default undefined; // Change me!
